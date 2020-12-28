7b64948062155c416f2efec9afcf8c8db4de4a66
Sam Sheets <sam.sheets94@gmail.com>
2020-12-28 14:57:28 -0700

    query object and morgan middleware

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 
 //creates the application
 const app = express()
+//mount Morgan
+app.use(morgan('dev'))
 
 //sending some text to the user with a GET request to the root URL
 app.get('/', (req, res) => {
@@ -18,6 +21,33 @@ app.get('/pizza/pepperoni', (req, res) => {
 
 app.get('/pizza/pineapple', (req, res) => {
     res.send('We don\'t serve that here. Never call again!')
+})
+
+app.get('/echo', (req, res) => {
+    const responseText = `Here are some details of your request:
+        Base URL: ${req.baseURL}
+        Host: ${req.hostname}
+        Path: ${req.path}`
+
+    res.send(responseText)
+})
+
+app.get('/queryViewer', (req, res) => {
+    console.log(req.query)
+    res.end();
+})
+
+app.get('/greetings', (req, res) => {
+    const name = req.query.name
+    const race = req.query.race
+    if(!name) {
+        return res.status(400).send('Please provide a name')
+    }
+    if(!race) {
+        return res.status(404).send('Please provide a race')
+    }
+    const greeting = `Greetings ${name} the ${race}, welcome to our kingdom.`
+    res.send(greeting)
 })
 
 //listening on local host 8000
